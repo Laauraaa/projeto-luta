890977a1d7db324ec5e459dfd238bc8c0ea2ac3a
Laura <140553486+Laauraaa@users.noreply.github.com>
2024-07-26 18:22:42 -0300

    edições no script

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,57 @@
-// função para clicar nas imagens e preencer as div's do pvp area
-// primeiro vou definir uma variavel qualquer como = 0, quando o usuário clicar no primeiro personagem essa variavel vai receber 1, é quando essa variavel for 1 o segundo click vai direcionar o segundo personagem para a segunda div
-// forEach como funciona : EXEMPLO 
-//[Nome do array].forEach(function(item){
-//if (item.age > 18){
-//console.log(blablabla)}
-//}
-//) Dentro dessa função, image representa o elemento atual.
-
-
-
-/*let clickCount = 0;
-
-function select_characters() {
-    clickCount++;
-
-        if (clickCount === 1) {
-            document.getElementById('fighter_1').innerHTML = `<img src="${this.src}" alt="${this.alt}" />`;
-
-        } else if (clickCount === 2) {
-            document.getElementById('fighter_2').innerHTML = `<img src="${this.src}" alt="${this.alt}" />`;
-
-
-        }
-}
-
-document.querySelectorAll('.image-click').forEach(image => {image.addEventListener('click', select_characters)}); */
-
-function selection (fighter) {
+// função para por a imagem do fighter na div vazia
+//char 1
+function selection_1 (fighter) {
     console.log('voce escolheu', fighter)
     document.querySelector('#fighter_1').innerHTML = `<img src="images/${fighter}.jpg"/>`
+}
+//char 2
+function selection_2 (fighter) {
+    console.log('voce escolheu', fighter)
+    document.querySelector('#fighter_2').innerHTML = `<img src="images/${fighter}.jpg"/>`
 }
 
 // Definindo os personagens 
 let char1 = ''
 let char2 = ''
 
+//char 1
 document.querySelector('#img-fighter-gun').addEventListener('click', () => {
-    selection ('gun')
+    selection_1 ('gun')
     char1 = 'gun'
 } )
 
 document.querySelector('#img-fighter-box').addEventListener('click', () => {
-    selection ('box')
+    selection_1 ('box')
     char1 = 'box'
 } )
 
 document.querySelector('#img-fighter-samurai').addEventListener('click', () => {
-    selection ('samurai')
+    selection_1 ('samurai')
     char1 = 'samurai'
 } )
 
 document.querySelector('#img-fighter-karate').addEventListener('click', () => {
-    selection ('karate')
+    selection_1 ('karate')
     char1 = 'karate'
+} )
+
+//char 2
+document.querySelector('#img-fighter-gun-2').addEventListener('click', () => {
+    selection_2 ('gun')
+    char2 = 'gun'
+} )
+
+document.querySelector('#img-fighter-box-2').addEventListener('click', () => {
+    selection_2 ('box')
+    char2 = 'box'
+} )
+
+document.querySelector('#img-fighter-samurai-2').addEventListener('click', () => {
+    selection_2 ('samurai')
+    char2 = 'samurai'
+} )
+
+document.querySelector('#img-fighter-karate-2').addEventListener('click', () => {
+    selection_2 ('karate')
+    char2 = 'karate'
 } )
