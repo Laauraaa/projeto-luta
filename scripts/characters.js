16a9a84a58befe4c59a87f0a675b70c0622f004a
Laura <140553486+Laauraaa@users.noreply.github.com>
2024-08-13 11:04:55 -0300

    algoritmo adicionado

--- scripts/characters.js
+++ scripts/characters.js
@@ -1,54 +1,57 @@
-class Character {
-    life = 0
-    defense = 0
-    agility = 0
-    attack = 0
-    magic_damage = 0
-    constructor (name) {
-        this.name = name
+const defaultCharacter = {
+    name:'',
+    life:0,
+    defense:0,
+    agility:0,
+    attack:0,
+    magic_damage:0
+}
+
+
+const createGun = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life:14,
+        defense:6,
+        agility:36,
+        attack:28,
+        magic_damage:16
     }
 }
 
-class Gun extends Character {
-    constructor(name){
-        super(name)
-        life = 14
-        defense = 6
-        agility = 36
-        attack = 28
-        magic_damage = 16
+const createBox = (name) =>  {
+    return {
+        ...defaultCharacter,
+        name,
+        life:30,
+        defense:23,
+        agility:18,
+        attack:25,
+        magic_damage:3
     }
 }
 
-class Box extends Character {
-    constructor(name){
-        super(name)
-        life = 30
-        defense = 23
-        agility = 18
-        attack = 25
-        magic_damage = 3
+const createSamurai = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life:25,
+        defense:20,
+        agility:25,
+        attack:10,
+        magic_damage:15
     }
 }
 
-class Samurai extends Character {
-    constructor(name){
-        super(name)
-        life = 25
-        defense = 20
-        agility = 25
-        attack = 10
-        magic_damage = 15
-    }
-}
-
-class Karate extends Character {
-    constructor(name){
-        super(name)
-        life = 24
-        defense = 25
-        agility = 25
-        attack = 25
-        magic_damage = 1
+const createKarate = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life:24,
+        defense:25,
+        agility:25,
+        attack:25,
+        magic_damage:1
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
 // função para por a imagem do fighter na div vazia
 //char 1
 function selection_1 (fighter) {
-    console.log('voce escolheu', fighter)
     document.querySelector('#fighter_1').innerHTML = `<img src="images/${fighter}.jpg"/>`
 }
 //char 2
 function selection_2 (fighter) {
-    console.log('voce escolheu', fighter)
     document.querySelector('#fighter_2').innerHTML = `<img src="images/${fighter}.jpg"/>`
 }
 
@@ -17,43 +15,98 @@ let char2 = ''
 //char 1
 document.querySelector('#img-fighter-gun').addEventListener('click', () => {
     selection_1 ('gun')
-    char1 = 'gun'
+    char1 = createGun('gun')
 } )
 
 document.querySelector('#img-fighter-box').addEventListener('click', () => {
     selection_1 ('box')
-    char1 = 'box'
+    char1 = createBox('Box')
 } )
 
 document.querySelector('#img-fighter-samurai').addEventListener('click', () => {
     selection_1 ('samurai')
-    char1 = 'samurai'
+    char1 = createSamurai('samurai')
 } )
 
 document.querySelector('#img-fighter-karate').addEventListener('click', () => {
     selection_1 ('karate')
-    char1 = 'karate'
+    char1 = createKarate('karate')
 } )
 
 //char 2
 document.querySelector('#img-fighter-gun-2').addEventListener('click', () => {
     selection_2 ('gun')
-    char2 = 'gun'
+    char2 = createGun('gun')
 } )
 
 document.querySelector('#img-fighter-box-2').addEventListener('click', () => {
     selection_2 ('box')
-    char2 = 'box'
+    char2 = createBox('box')
 } )
 
 document.querySelector('#img-fighter-samurai-2').addEventListener('click', () => {
     selection_2 ('samurai')
-    char2 = 'samurai'
+    char2 = createSamurai('samurai')
 } )
 
 document.querySelector('#img-fighter-karate-2').addEventListener('click', () => {
     selection_2 ('karate')
-    char2 = 'karate'
+    char2 = createKarate('karate')
 } )
+
+// button play
+document.querySelector('.button').addEventListener('click', () => {play()})
+
+function play() {
+    let char1victory = 0
+    let char2victory = 0
+    //life
+  if (char1.life > char2.life) {
+    char1victory++
+  } else if (char1.life < char2.life) {
+    char2victory++
+  } 
+  //defense
+  if (char1.defense > char2.defense) {
+    char1victory++
+  } else if (char1.defense < char2.defense) {
+    char2victory++
+  }
+  //agility
+  if (char1.agility > char2.agility) {
+    char1victory++
+  } else if (char1.agility < char2.agility) {
+    char2victory++
+  }
+  //attack
+  if (char1.attack > char2.attack) {
+    char1victory++
+  } else if (char1.attack < char2.attack) {
+    char2victory++
+  }
+  //magic damage
+  if (char1.magic_damage > char2.magic_damage) {
+    char1victory++
+  } else if (char1.magic_damage < char2.magic_damage) {
+    char2victory++
+  }
+  //
+  if (char1victory === char2victory) {
+    char1victory *= Math.random()
+    char2victory *= Math.random()
+  }
+  //
+   if (char1victory > char2victory) {
+    document.querySelector('#result-text').innerHTML = `${char1.name} venceu`
+  } else if (char1victory < char2victory) {
+    document.querySelector('#result-text').innerHTML = `${char2.name} venceu`
+  } else {
+    document.querySelector('#result-text').innerHTML = `Houve um empate, selecione personagens diferentes.`
+  }
+} 
+/*function points(char1victory, char2victory){
+    console.log(char1victory)
+    console.log(char2victory)
+}*/
 
 
